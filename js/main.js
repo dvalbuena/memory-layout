bd732914fb671c4e40340166ab875bb78f56e6e0
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-19 12:27:05 -0500

    flipping all boxes

--- js/main.js
+++ js/main.js
@@ -4,11 +4,6 @@ $(function () {
 // yay for multiple classes - hopefully this will help in selecting the class open
 // and keeping boxes open if they match
 
-// $('.fa.fa-fort-awesome.fa-2x.open').on('click', function(){
-//   $(this).animate({
-//     rotateY(180deg);
-//   });
-
 // this rotates the icon 180 deg
 // $('.fa.fa-fort-awesome.fa-2x.open').on('click', function(){
 //   $(this).css({
@@ -16,11 +11,190 @@ $(function () {
 //   });
 // });
 
-$('.card').on('click', function(){
-  $(this).css({
-    'transform': 'rotateY(180deg)',
+// $(function(){
+
+  $('#fort-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#fort1').css({
+      'color': '#1d9d74'
+    });
   });
-});
+
+  $('#modx-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#modx1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#codiepie-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#codiepie1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#bag-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#bag1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#fort-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#fort2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#beer-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#beer1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#cake-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#cake1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#smiley-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#smiley1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#hashtag-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#hashtag1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#anchor-one').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#anchor1').css({
+      'color': '#1d9d74'
+    });
+  });
+
+
+  $('#bag-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#bag2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#modx-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#modx2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#smiley-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#smiley2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#beer-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#beer2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#anchor-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#anchor2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#cake-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#cake2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#hashtag-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#hashtag2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+  $('#codiepie-two').on('click', function(){
+    $(this).css({
+      'transform': 'rotateY(180deg)',
+      'transition': 'background 1s, -webkit-transform 1s'
+    });
+    $('#codiepie2').css({
+      'color': '#1d9d74'
+    });
+  });
+
+
 
 
 
